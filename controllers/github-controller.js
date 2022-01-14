14351c3d9bb4bc349cbc6fcfd35e9666261809ca
grantfogle <grantjfogle@gmail.com>
2022-01-14 13:00:13 -0700

    added method to clean up url submitted from user with form

--- controllers/github-controller.js
+++ controllers/github-controller.js
@@ -1,6 +1,25 @@
-function fetchOpenPrs(gitRepoUrl) {
-    console.log(gitRepoUrl)
+function cleanUpUrl(repoUrl) {
+    let cleanedUpUrl = repoUrl;
+
+    if (!cleanedUpUrl.includes('repos')) {
+        cleanedUpUrl = cleanedUpUrl.replace('.com', '.com/repos');
+    }
+
+    if (!cleanedUpUrl.includes("https://")) {
+        cleanedUpUrl = "https://api." + cleanedUpUrl;
+    } else {
+        cleanedUpUrl = cleanedUpUrl.replace("https://", "https://api.");
+    }
+
+    if (cleanedUpUrl.charAt(cleanedUpUrl.length - 1) !== "/") {
+        cleanedUpUrl += "/";
+    }
+    return cleanedUpUrl += "pulls?q=is%3Aopen";
 }
 
+function fetchOpenPrs(gitRepoUrl) {
+    const cleanFetchUrl = cleanUpUrl(gitRepoUrl);
+    console.log(cleanFetchUrl)
+}
 
-module.exports = { fetchOpenPrs };
+module.exports = { fetchOpenPrs, cleanUpUrl };
