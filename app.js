a04a0f6dd1d32ddba94018c3fbfdc85426b9c09e
grantfogle <grantjfogle@gmail.com>
2022-01-14 12:43:11 -0700

    adding basic home screen with header and form, plus some bootstrap styling

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
 const PORT = 8080;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send('<h1>HelloWorld</h1>')
+    res.render("home", {
+        title: "Grant's PR Fetcher",
+        openPrs: []
+    });
 });
 
 app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
